3e5447e85d036b882a673a3eb85b55ccacd54aec
Danil-Zaichkin <91212875+Danil-Zaichkin@users.noreply.github.com>
2022-02-27 17:25:52 +0500

    Update 1.cpp

--- struct(27.02.22)/1.cpp
+++ struct(27.02.22)/1.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+struct Student {
+    int age;
+    string name;
+    string drink;
+};
+
+int main() {
+    int num;
+    cin >> num;
+    Student* mas = new Student[num];
+
+    for (int i = 0; i < num; i++) {
+        cin >> mas[i].age >> mas[i].name >> mas[i].drink;
+    }
+
+    for (int i = 0; i < num; i++) {
+        cout << mas[i].age << ' ' << mas[i].name << ' ' << mas[i].drink << endl;
+    }
+}
