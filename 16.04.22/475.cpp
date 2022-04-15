7bb874160d3c37e54b569213e34fc20d59742b59
Danil-Zaichkin <91212875+Danil-Zaichkin@users.noreply.github.com>
2022-04-16 00:18:52 +0500

    Create 475.cpp

---
+++ 16.04.22/475.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream fileIn("input.txt");
+    ofstream fileOut("output.txt");
+
+    vector<int> vec;
+    int tmp;
+    while (!fileIn.eof()) {
+        fileIn >> tmp;
+        vec.push_back(tmp);
+    }
+
+    sort(vec.begin(), vec.end());
+
+    int d = vec[1] - vec[0];
+    for (int i = 1; i < vec.size() - 1; ++i) {
+        if (vec[i + 1] - vec[i] != d) {
+            fileOut << "No";
+            return 0;
+        }
+    }
+    fileOut << "Yes";
+    return 0;
+}
