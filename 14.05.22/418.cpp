76334f8edb22c511f23a7dbd840b9a7888e939e3
Danil-Zaichkin <91212875+Danil-Zaichkin@users.noreply.github.com>
2022-05-14 00:58:28 +0500

    Update 418.cpp

--- 14.05.22/418.cpp
+++ 14.05.22/418.cpp
@@ -1,1 +1,44 @@
 
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+
+    int curr = 0;
+    int mx = 0;
+
+    string str;
+    getline(cin, str);
+
+    vector<int> mas;
+    mas.push_back(0);
+
+    for (char i : str) {
+        if (i == '<') {
+            if (mas[curr] > 0)
+                mas[curr]--;
+            else if (curr > 0)
+                mas.erase(mas.begin() + curr--);
+        } else if (i == '^') {
+            if (curr > 0)
+                curr--;
+        } else if (i == '|') {
+            if (curr + 1 < mas.size())
+                ++curr;
+        } else if (i == '\\') {
+            mas.push_back(0);
+            ++curr;
+        } else {
+            mas[curr]++;
+        }
+        mx = max(mx, mas[curr]);
+    }
+
+    cout << mx;
+
+    return 0;
+}
